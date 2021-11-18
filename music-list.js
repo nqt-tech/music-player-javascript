c028380b475441faa750acdf7380145178396fd0
Sharecs <sharecscongnghe@gmail.com>
2021-11-18 09:33:40 +0700

    Sharecs
    submit

---
+++ music-list.js
@@ -0,0 +1,47 @@
+// Để thêm bài nhạc mình sẽ them vào list sau
+
+//   {
+//     name: "Tên bài nhạc",
+//     artist: "Nghệ sĩ",
+//     img: "Tên ảnh định dạng .jpg trong project",
+//     src: "Link đường dẫn file nhạc đinh dạng .mp3"
+//   }
+
+let allMusic = [
+  {
+    name: "Ai là người thương em",
+    artist: "Quân A.P",
+    img: "Ai-La-Nguoi-Thuong-Em-Quan-A-P",
+    src: "Ai-La-Nguoi-Thuong-Em-Quan-A-P"
+  },
+  {
+    name: "Đôi ba chiếc lá",
+    artist: "Khói",
+    img: "Doi_Ba_Chiec_La_Khoi",
+    src: "Doi_Ba_Chiec_La_Khoi"
+  },
+  {
+    name: "Gió vẫn hát",
+    artist: "Long Phạm",
+    img: "Gio-Van-Hat-Long-Pham",
+    src: "Gio-Van-Hat-Long-Pham"
+  },
+  {
+    name: "Hồng Nhan",
+    artist: "Jack",
+    img: "Hong-Nhan-Jack",
+    src: "Hong-Nhan-Jack"
+  },
+  {
+    name: "Ừ có anh đây",
+    artist: "Tino",
+    img: "U-Co-Anh-Day-Tino",
+    src: "U-Co-Anh-Day-Tino"
+  },
+  {
+    name: "Vì anh là gió",
+    artist: "Lilk",
+    img: "ViAnhLaGio-LilK-4766315",
+    src: "ViAnhLaGio-LilK-4766315"
+  },
+];
